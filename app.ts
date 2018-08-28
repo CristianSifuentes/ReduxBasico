df2ad60339e6fe67a826d3f121d7bb76ed3529ac
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-08-28 13:00:01 -0500

    CSC 2018/08/28: Entender redux

--- app.ts
+++ app.ts
@@ -12,21 +12,48 @@ interface Action {
 
 //reducer -> regresa un nuevo estado
 //siempre regresan un estado
+//Todo lo que el reducer realice debe ser pura
 function reducer(state = 10, action: Action){
 
-   if(action.type === 'INCREMENTADOR'){
-       return state += 1;
-   }
+  switch(action.type){
+      case 'INCREMENTADOR':
+         return state += 1;
+      case 'DECREMENTAR':
+         return state -= 1;
+      case 'MULTIPLICAR':
+         return state * action.payload
+      case 'DIVIDIR':
+         return state / action.payload
+      default:
+        return state;
+  }
+
 
    return state;
 }
-
-// Usar el reducer
 
 
  const incrementadorAction: Action = {
         type: 'INCREMENTADOR'
  }
 
+ const decrementadorAction: Action = {
+       type: 'DECREMENTAR'
+ }
+
+ const multiplicarAction: Action = {
+    type: 'MULTIPLICAR',
+    payload: 7
+}
+
+
+const dividirAction: Action = {
+    type: 'DIVIDIR',
+    payload: 7
+}
+
 
 console.log(reducer(10, incrementadorAction));
+console.log(reducer(10, decrementadorAction));
+console.log(reducer(10, multiplicarAction));
+console.log(reducer(10, dividirAction));
